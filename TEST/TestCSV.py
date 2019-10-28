41185381e526c9cd919a150c296d043978f7f746
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2019-10-28 20:02:07 +0800

    still testing

---
+++ TEST/TestCSV.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import math
+
+data = np.random.randint(1,100,(5,5))
+df = pd.DataFrame(data,  columns=list('ABCDE'))
+#print(df)
+df.to_csv("TestCSV.csv")
+df_load = pd.read_csv("TestCSV.csv",index_col = 0)
+#print(df_load)
+
+
+#Data structure List:
+
+TimeList = list() # = SampleList = []
+for i in range(0,300):
+    TimeList.append(i)
+#print(TimeList)
+RSSIList = []
+for i in range(0,300):
+    RSSIList.append( math.log(10,10) )
+print(RSSIList)
+
+    
