3a6dbdaae069d4c96047bd7723761c706f9ad01b
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2019-10-30 19:04:14 +0800

    output AttributesTable as CSV file.

--- TEST/SVMImplementation.py
+++ TEST/SVMImplementation.py
@@ -3,31 +3,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-
+import math
 #Input Data
 
-InitialDistance = float(input("plz input the initial distance between Alice and Bob:"))
+InitialDistance = float(input("plz input the initial distance between Alice and Bob:"))#unit
 #initial value of drone
-InitialPower = 10.0 #unit:dBm
-
-
-
-def PathLoss(distance):
-    return 75.0+36.1*( math.log( distance/10.0 ) )
-
-PL = []
-
-CurrentDistance = InitialDistance
-RSSI = []
-for i in range(0,300):
-    RSSI.append( ( InitialPower*( 10.0**( -( PathLoss( CurrentDistance + 1/3.0 )/10.0 ) ) ) )-( InitialPower*( 10**( -( PathLoss( CurrentDistance )/10.0 ) ) ) ) )
-    CurrentDistance = CurrentDistance + 1/3.0
-print(RSSI)
-
-
-
+InitialPower = 10.0 #unit:dBm = 10*log(W/mW)
 
 
 #Data Preprocessing
+
+
+#RSSI List and CurrentDistanceList Setup
+#RSSI Path Loss Calculation
+def PathLoss(d):
+    return 75.0 + 36.1*( math.log( d/10, 10.0 ) )
+#Sampling every 0.3m 
+CurrentDistance = InitialDistance
+RSSIList = []
+CurrentDistanceList = []#In case
+for i in range(0,300):
+    RSSIList.append( PathLoss(CurrentDistance) - PathLoss(CurrentDistance + 1/3.0) )
+    CurrentDistanceList.append(CurrentDistance)
+    CurrentDistance = CurrentDistance + 1/3.0
+
+#Attribute Table setup as DataFrame
+AttributesTable = {"RSSI": RSSIList}
+AttributesTable = pd.DataFrame(AttributesTable)
+#Output as CSV
+AttributesTable.to_csv("Test.csv")
+
+
+
+
+
+
+
+
+
 
 
