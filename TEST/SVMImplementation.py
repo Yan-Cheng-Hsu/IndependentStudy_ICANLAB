ceb1b04dbe68f848966f54e0c09d44abef9906a0
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2019-10-30 18:21:19 +0800

    fixed math domain problem in log()function.

--- TEST/SVMImplementation.py
+++ TEST/SVMImplementation.py
@@ -1,5 +1,33 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-import math
+
+#Input Data
+
+InitialDistance = float(input("plz input the initial distance between Alice and Bob:"))
+#initial value of drone
+InitialPower = 10.0 #unit:dBm
+
+
+
+def PathLoss(distance):
+    return 75.0+36.1*( math.log( distance/10.0 ) )
+
+PL = []
+
+CurrentDistance = InitialDistance
+RSSI = []
+for i in range(0,300):
+    RSSI.append( ( InitialPower*( 10.0**( -( PathLoss( CurrentDistance + 1/3.0 )/10.0 ) ) ) )-( InitialPower*( 10**( -( PathLoss( CurrentDistance )/10.0 ) ) ) ) )
+    CurrentDistance = CurrentDistance + 1/3.0
+print(RSSI)
+
+
+
+
+
+#Data Preprocessing
+
+
--- TEST/TestSklearn.py
+++ TEST/TestSklearn.py
@@ -85,3 +85,4 @@ Ytable = pd.Series(Y)
 
 
 
+                                              
